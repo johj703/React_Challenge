5a05b254c66a0d6d6d86b1c6348ec2af01fa407f
Jo <johj703@nate.com>
2024-02-24 22:26:56 +0900

    App.tsx 수정

--- Marvelous_React/src/App.tsx
+++ Marvelous_React/src/App.tsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import React from "react";
+import { Browser as Routes, Route } from "react-router-dom";
+import CharacterList from "./Components/CharacterList";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<CharacterList />} />
+    </Routes>
+  );
 }
 
 export default App;
