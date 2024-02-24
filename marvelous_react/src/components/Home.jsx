8879d90ee4cfb738033cec1760ecb1da27fa104f
Jo <johj703@nate.com>
2024-02-25 00:05:09 +0900

    Update Home.jsx

--- marvelous_react/src/components/Home.jsx
+++ marvelous_react/src/components/Home.jsx
@@ -4,10 +4,11 @@ function Home() {
   const [loading, setLoading] = useState(true);
   const [heros, setHeros] = useState([]);
   const getHeros = async () => {
-    const response = await fetch(
-      `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
-    );
-    const json = await response.json();
+    const json = await (
+      await fetch(
+        `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
+      )
+    ).json();
     setHeros(json.data.results);
     setLoading(false);
   };
