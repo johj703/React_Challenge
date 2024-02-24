795c62242889cc7dab36372860b3a5840a854f78
Jo <johj703@nate.com>
2024-02-24 23:44:36 +0900

    useEffect 작성

--- marvelous_react/src/components/Home.jsx
+++ marvelous_react/src/components/Home.jsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Home() {
   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(
+      `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
+    ).then((response) => response.json());
+  }, []);
   return <div>{loading ? <h1>Loading</h1> : null}</div>;
 }
 
