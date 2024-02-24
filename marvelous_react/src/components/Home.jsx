fcce50a6f9d286f252616b4be956f01d5bcb53a2
Jo <johj703@nate.com>
2024-02-24 23:47:57 +0900

    response 작성 및 app에 Home 컴포넌트 등록

--- marvelous_react/src/components/Home.jsx
+++ marvelous_react/src/components/Home.jsx
@@ -5,7 +5,9 @@ function Home() {
   useEffect(() => {
     fetch(
       `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
-    ).then((response) => response.json());
+    )
+      .then((response) => response.json())
+      .then((json) => console.log(json));
   }, []);
   return <div>{loading ? <h1>Loading</h1> : null}</div>;
 }
