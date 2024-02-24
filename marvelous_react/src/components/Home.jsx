7becc5100c05d3a4303f2ae1b75fff9bd3e72b17
Jo <johj703@nate.com>
2024-02-24 23:56:04 +0900

    setHeros를 통해서 영웅들 리스트 데이터를 불러오기

--- marvelous_react/src/components/Home.jsx
+++ marvelous_react/src/components/Home.jsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 
 function Home() {
   const [loading, setLoading] = useState(true);
+  const [heros, setHeros] = useState([]);
   useEffect(() => {
     fetch(
       `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
     )
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => setHeros(json.data.results));
   }, []);
+  console.log(heros);
   return <div>{loading ? <h1>Loading</h1> : null}</div>;
 }
 
