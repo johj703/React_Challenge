37a11b542cdef4705bf439bbe073c835b3812a6f
Jo <johj703@nate.com>
2024-02-24 22:15:23 +0900

    resolve 추가

--- Marvelous_React/vite.config.ts
+++ Marvelous_React/vite.config.ts
@@ -1,3 +1,5 @@
+import * as path from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
@@ -7,5 +9,10 @@ export default defineConfig({
   plugins: [react(), svgrPlugin()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
