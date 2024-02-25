8f9111d896a5d201857b27666ad1a25186c7a0b8
Jo <johj703@nate.com>
2024-02-25 15:24:30 +0900

    heros 상태 만들고 setHeros로 데이터 결과 나타내기

--- marvelous_react/src/App.js
+++ marvelous_react/src/App.js
@@ -2,12 +2,13 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState();
+  const [heros, setHeros] = useState([]);
   useEffect(() => {
     fetch(
       `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
     )
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => setHeros(json.data.results));
   }, []);
   return <div>{loading ? <h1>Loading</h1> : null}</div>;
 }
