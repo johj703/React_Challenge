b17a1898f935ecaa5db1f20882b2ceff43c871b1
Jo <johj703@nate.com>
2024-02-25 15:19:43 +0900

    useEffect fetch로 영웅들 정보 받아오기

--- marvelous_react/src/App.js
+++ marvelous_react/src/App.js
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState();
+  useEffect(() => {
+    fetch(
+      `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
+    )
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
   return <div>{loading ? <h1>Loading</h1> : null}</div>;
 }
 
