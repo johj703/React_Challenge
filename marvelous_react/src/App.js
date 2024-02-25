5975b36ed5d00193f9d3fd8711220a8fed07d5c0
Jo <johj703@nate.com>
2024-02-25 20:05:50 +0900

    라우터에 안에 페이지들 입력하기

--- marvelous_react/src/App.js
+++ marvelous_react/src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
