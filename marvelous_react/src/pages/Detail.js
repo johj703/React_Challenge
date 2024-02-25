91e8cab41e95a3073ed035fbddc4dce6a9192cb8
Jo <johj703@nate.com>
2024-02-25 22:18:40 +0900

    Detail 페이지 초기 모습

--- marvelous_react/src/pages/Detail.js
+++ marvelous_react/src/pages/Detail.js
@@ -1,8 +1,19 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 function Detail() {
   const { id } = useParams();
-  console.log(id);
+  const getHeros = async () => {
+    const json = await (
+      await fetch(
+        `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters/${id}`
+      )
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getHeros();
+  }, []);
   return <h1>Detail</h1>;
 }
 
