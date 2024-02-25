d2ff135b0edd543186d96fc0f22912b6d6e55c72
Jo <johj703@nate.com>
2024-02-25 20:31:38 +0900

    이름을 클릭하면 해당 영웅들의 Detail 페이지로 넘어가도록 Link 컴포넌트 사용

--- marvelous_react/src/components/Heros.jsx
+++ marvelous_react/src/components/Heros.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Heros({ id, thumbnail, name }) {
   return (
     <div>
       <img src={thumbnail} alt="Heros thumbnail" />
-      <h2>{name}</h2>
+      <Link to={`/heros/${id}`}>{name}</Link>
     </div>
   );
 }
