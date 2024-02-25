c626e9c7354db5621fd485dd8b8cda5e91984d4a
Jo <johj703@nate.com>
2024-02-25 18:05:27 +0900

    App.js에 있던 코드를 Home.js 코드로 분리

--- marvelous_react/src/App.js
+++ marvelous_react/src/App.js
@@ -1,39 +1,5 @@
-import { useState, useEffect } from "react";
-import Heros from "./component/Heros.jsx";
-
 function App() {
-  const [loading, setLoading] = useState();
-  const [heros, setHeros] = useState([]);
-  const getHeros = async () => {
-    const json = await (
-      await fetch(
-        `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
-      )
-    ).json();
-    setHeros(json.data.results);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getHeros();
-  }, []);
-  // const thumbnailPath = `${heros.thumbnail.path}.${heros.thumbnail.extension};`;
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading</h1>
-      ) : (
-        <div>
-          {heros.map((hero) => (
-            <Heros
-              key={hero.id}
-              thumbnail={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
-              name={hero.name}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
