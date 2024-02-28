f0c39c98b2831146fec68144a02885e95768e973
Jo <johj703@nate.com>
2024-02-28 14:39:22 +0900

    prepend 함수 작성
    prepend(arr, item): 이 함수는 배열의 시작 부분에 item을 넣고 return해야 합니다.

--- Gemerics.ts
+++ Gemerics.ts
@@ -7,3 +7,10 @@ const last: JoSignatures1 = (arr) => {
   const arrLength = arr.length;
   return arr[arrLength - 1];
 };
+
+// 2번 prepend(arr, item)
+interface Prepend {
+  <T>(arr: T[], item: T): T[];
+}
+
+const prepend: Prepend = (arr, item) => [item, ...arr];
