a1bcc961920a45dfa3b2dfdd7d593f0e915e37a6
Jo <johj703@nate.com>
2024-03-06 18:33:04 +0900

    Link 컴포넌트 사용해서 Book Detail 페이지로 가는 라우터 생성

--- routing_lab/src/screens/Home.tsx
+++ routing_lab/src/screens/Home.tsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <>
       <h1>Best Seller Authors</h1>
+      <ul>
+        <li>
+          <Link to={"/"}>J.K.Rowling</Link>
+        </li>
+        <li>
+          <Link to={"/about"}>J.R.R. Tolkien</Link>
+        </li>
+      </ul>
     </>
   );
 }
