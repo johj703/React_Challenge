d5d92ae49a1acaf9161cdbf9e005f74cf90858c8
Jo <johj703@nate.com>
2024-03-06 17:18:50 +0900

    부모 태그를 header로 변경

--- routing_lab/src/components/Header.tsx
+++ routing_lab/src/components/Header.tsx
@@ -2,10 +2,16 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/about"}>About</Link>
-    </>
+    <header>
+      <ul>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/about"}>About</Link>
+        </li>
+      </ul>
+    </header>
   );
 }
 export default Header;
