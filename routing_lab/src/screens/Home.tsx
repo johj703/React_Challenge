8618f0758ec771e02e3b8f4294ec108d330062a3
Jo <johj703@nate.com>
2024-03-06 18:47:02 +0900

    map을 사용해서 db에 있는 Author의 정보 보여주기

--- routing_lab/src/screens/Home.tsx
+++ routing_lab/src/screens/Home.tsx
@@ -1,16 +1,13 @@
-import { Link } from "react-router-dom";
+import { Authors } from "../db";
 
 function Home() {
   return (
     <>
       <h1>Best Seller Authors</h1>
       <ul>
-        <li>
-          <Link to={"/"}>J.K.Rowling</Link>
-        </li>
-        <li>
-          <Link to={"/about"}>J.R.R. Tolkien</Link>
-        </li>
+        {Authors.map((Authors) => (
+          <li key={Authors.id}>{Authors.name}</li>
+        ))}
       </ul>
     </>
   );
