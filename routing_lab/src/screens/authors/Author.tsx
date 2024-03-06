2b5dd28a96ef771063d9010feb74169f0f587480
Jo <johj703@nate.com>
2024-03-06 23:41:39 +0900

    Best Seller Authors에서 작가를 클릭하면 '작가를 찾을 수 없어요!'가 뜸.

--- routing_lab/src/screens/authors/Author.tsx
+++ routing_lab/src/screens/authors/Author.tsx
@@ -1,7 +1,16 @@
 import { useParams } from "react-router-dom";
+import { Database } from "../../db";
 
 function Author() {
-  const { authorId } = useParams();
-  return <h1>Author</h1>;
+  const { name } = useParams();
+  const writer = Database.find((writer) => writer.name === name);
+  if (!writer) {
+    return <div>작가를 찾을 수 없어요!</div>;
+  }
+  return (
+    <div>
+      <h1>{writer.name}</h1>
+    </div>
+  );
 }
 export default Author;
