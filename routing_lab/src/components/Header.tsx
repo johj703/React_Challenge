1f206e3dc333c8c668827c0a7e5a0f2a6e7b7abe
Jo <johj703@nate.com>
2024-03-06 17:11:25 +0900

    Link를 사용해서 각 페이지로 이동할 수 있도록 작성

--- routing_lab/src/components/Header.tsx
+++ routing_lab/src/components/Header.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 function Header() {
-  return <></>;
+  return (
+    <>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/about"}>About</Link>
+    </>
+  );
 }
