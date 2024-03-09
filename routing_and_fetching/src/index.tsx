3f51756d92bae33601e122a603d0062e3903aff3
Jo <johj703@nate.com>
2024-03-09 23:37:40 +0900

    index 수정

--- routing_and_fetching/src/index.tsx
+++ routing_and_fetching/src/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ThemeProvider } from "styled-components";
 import App from './App';
+import { theme } from "./theme";
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
