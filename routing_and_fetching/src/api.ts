4ce0657c9019e074e2ffb742dc69d040170a914a
Jo <johj703@nate.com>
2024-03-10 01:00:50 +0900

    선택한 캐릭터들의 상세 정보를 fetch해서 불러올 함수 작성

--- routing_and_fetching/src/api.ts
+++ routing_and_fetching/src/api.ts
@@ -3,3 +3,7 @@ const BASE_URL = `https://disney_api.nomadcoders.workers.dev/`;
 export function fetchCharacters() {
     return fetch(`${BASE_URL}/characters`).then((response) => response.json());
 }
+
+export function fetchCharactersInfo(characterId: string) {
+    return fetch(`${BASE_URL}/characters/${characterId}`).then((response) => response.json());
+}
