37a3d9af5b2c34e777ab30ea9e17a2b9a72b8776
Jo <johj703@nate.com>
2024-03-10 14:13:38 +0900

    BASE_URL에 "/"제거해서 데이터 받아올 수 있게 작성

--- routing_and_fetching/src/api.ts
+++ routing_and_fetching/src/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = `https://disney_api.nomadcoders.workers.dev/`;
+const BASE_URL = `https://disney_api.nomadcoders.workers.dev`;
 
 export function fetchCharacters() {
     return fetch(`${BASE_URL}/characters`).then((response) => response.json());
