f63f2fc5fc0ec9920e66cab5773cc09d4f5a2d76
Jo <johj703@nate.com>
2024-03-10 23:23:45 +0900

    Detail 페이지 작성

--- routing_and_fetching/src/routes/Detail.tsx
+++ routing_and_fetching/src/routes/Detail.tsx
@@ -1,6 +1,48 @@
+import { useParams } from "react-router";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom"
+import { useQuery } from "react-query";
+import { fetchCharactersInfo } from "../api";
+
+const Container = styled.div`
+
+`;
+
+const Name = styled.div`
+`;
+
+const Film = styled.div`
+`;
+
+
+interface RouteParams {
+    id: number;
+    characters: string;
+    films: string[];
+    imageUrl: string;
+}
+
+interface ICharacter {
+    
+}
+
 function Detail() {
+    const { id } = useParams()
+    const { isLoading, data } = useQuery({queryKey: ["allCharacters"], queryFn: fetchCharactersInfo()});
+    const GoBack = () => {
+        useHistory.push('/Home'); //뒤로가기 기능
+    }
     return(
-        <h1>Detail</h1>
+        <Container>
+            <div onClick={GoBack}>&larr;</div>
+            {/* <img src={`https://disney_api.nomadcoders.workers.dev/characters/${characters.imageUrl}`} />
+            <Name>{charactersId?.name}</Name>
+            <Film>{charactersId.films.map((films) => (
+                <div>
+                    {films}
+                </div>
+            ))}</Film> */}
+        </Container>
     );
 }
 export default Detail;
