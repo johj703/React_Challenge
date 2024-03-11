cf786ec624f5ab49f913b6a6cbc5c098ae04a48c
Jo <johj703@nate.com>
2024-03-11 10:32:59 +0900

    완성하지 못한 디즈니 캐릭터

--- routing_and_fetching/src/routes/Detail.tsx
+++ routing_and_fetching/src/routes/Detail.tsx
@@ -1,48 +1,47 @@
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
 import { useQuery } from "react-query";
 import { fetchCharactersInfo } from "../api";
 
-const Container = styled.div`
+const Container = styled.div``;
 
-`;
+const Name = styled.div``;
 
-const Name = styled.div`
-`;
-
-const Film = styled.div`
-`;
-
+const Film = styled.div``;
 
 interface RouteParams {
-    id: number;
-    characters: string;
-    films: string[];
-    imageUrl: string;
+  characterId: string;
 }
 
 interface ICharacter {
-    
+  id: number;
+  characters: string;
+  films: string[];
+  imageUrl: string;
 }
 
 function Detail() {
-    const { id } = useParams()
-    const { isLoading, data } = useQuery({queryKey: ["allCharacters"], queryFn: fetchCharactersInfo()});
-    const GoBack = () => {
-        useHistory.push('/Home'); //뒤로가기 기능
-    }
-    return(
-        <Container>
-            <div onClick={GoBack}>&larr;</div>
-            {/* <img src={`https://disney_api.nomadcoders.workers.dev/characters/${characters.imageUrl}`} />
-            <Name>{charactersId?.name}</Name>
-            <Film>{charactersId.films.map((films) => (
-                <div>
-                    {films}
-                </div>
-            ))}</Film> */}
-        </Container>
-    );
+  const { characterId } = useParams<RouteParams>();
+  const { isLoading, data } = useQuery("allCharacters", () =>
+    fetchCharactersInfo(characterId)
+  );
+  const GoBack = () => {
+    useHistory.push("/Home"); //뒤로가기 기능
+  };
+  return (
+    <Container>
+      <div onClick={GoBack}>&larr;</div>
+      <img
+        src={`https://disney_api.nomadcoders.workers.dev/characters/${characterId.imageUrl}`}
+      />
+      <Name>{characterId?.name}</Name>
+      <Film>
+        {characterId.films.map((films) => (
+          <div>{films}</div>
+        ))}
+      </Film>
+    </Container>
+  );
 }
 export default Detail;
--- routing_and_fetching/src/routes/Home.tsx
+++ routing_and_fetching/src/routes/Home.tsx
@@ -1,35 +1,35 @@
-import { useQuery } from "@tanstack/react-query" 
+import { useQuery } from "react-query";
 import styled from "styled-components";
 import { fetchCharacters } from "../api";
 import { Link } from "react-router-dom";
 
 const Container = styled.div`
-    padding: 0px, 20px;
-    max-width: 480px;
-    margin: 0 auto;
+  padding: 0px, 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.div`
-    height: 10vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Title = styled.div`
-    font-size: 48px;
-    color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
 `;
 
 const Loader = styled.span`
-    text-align: center;
-    display: block;
+  text-align: center;
+  display: block;
 `;
 
 const CharacterList = styled.ul``;
 
 const Character = styled.li`
-    background-color: white;
+  background-color: white;
   color: ${(props) => props.theme.bgColor};
   border-radius: 15px;
   margin-bottom: 10px;
@@ -47,43 +47,47 @@ const Character = styled.li`
 `;
 
 const Img = styled.img`
-    width: 35px;
-    height: 35px;
-    margin-right: 10px;
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 interface ICharacter {
-    id: string,
-    name: string,
-    imageUrl:string
+  id: string;
+  name: string;
+  imageUrl: string;
 }
 
 function Home() {
-    const { isLoading, data } = useQuery<ICharacter[]>({queryKey: ["allCharacters"], queryFn: fetchCharacters()});
-    console.log(data);
-    return(
-        <Container>
-            <Header>
-                <Title>Disney Characters!</Title>
-            </Header>
-            {isLoading ? (<Loader>Loading!!!</Loader>) : (
-                <CharacterList>
-                    {data?.slice(0, 50).map((characters) => (
-                        <Character>
-                            <Link 
-                                to={{
-                                    pathname: `/characters/${characters.id}`,
-                                    state: { name: characters.name},
-                            }}>
-                                <Img
-                                    src={characters.imageUrl}    
-                                />
-                                {characters.name}
-                            </Link>
-                        </Character>
-                    ))}
-                </CharacterList>
-            )}
-        </Container>
-    );
+  const { isLoading, data } = useQuery<ICharacter[]>({
+    queryKey: ["allCharacters"],
+    queryFn: fetchCharacters(),
+  });
+  console.log(data);
+  return (
+    <Container>
+      <Header>
+        <Title>Disney Characters!</Title>
+      </Header>
+      {isLoading ? (
+        <Loader>Loading!!!</Loader>
+      ) : (
+        <CharacterList>
+          {data?.slice(0, 50).map((characters) => (
+            <Character>
+              <Link
+                to={{
+                  pathname: `/characters/${characters.id}`,
+                  state: { name: characters.name },
+                }}
+              >
+                <Img src={characters.imageUrl} />
+                {characters.name}
+              </Link>
+            </Character>
+          ))}
+        </CharacterList>
+      )}
+    </Container>
+  );
 }
 export default Home;
